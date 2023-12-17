60f73b24382b02cb33275d83549e9fda8c2ea60e
Anna Zalesińska <93271870+kashel-mp3@users.noreply.github.com>
2023-12-18 00:49:05 +0100

    Update exact.cpp
    Signed-off-by: Anna Zalesińska <93271870+kashel-mp3@users.noreply.github.com>

--- exact.cpp
+++ exact.cpp
@@ -1,5 +1,4 @@
 #include <nlohmann/json.hpp>
-
 #include <string>
 #include <iostream>
 #include <vector>
@@ -14,32 +13,36 @@ typedef std::vector<std::vector<int>> vec_vec_int;
 
 using json = nlohmann::json;
 
-std::string FILENAME = "test.json";
-
-void read_data_from_json(std::string &filename, int &n, int &s, std::vector<std::vector<int>> &graph, std::vector<int> &stop_vertices);
+void read_data_from_json(std::filesystem::path test_data_path, int &n, int &s, std::vector<std::vector<int>> &graph, std::vector<int> &stop_vertices);
 vec_int solve(int n, int s, vec_vec_int &graph, vec_int &stop_vertices, vec_bool &stop_vertices_check);
 void check_all_possible_paths(int pos, int cur_l, int max_l, int used_s, vec_int path, vec_bool visited, 
                             int& min_max_l, vec_int& opt_path, int n, int s, vec_bool& stops, vec_vec_int& graph);
 bool is_connected(int n, vec_vec_int &graph);
 void dfs(int n, int v, vec_vec_int &graph, std::vector<bool> &visited);
+int max_subpath(vec_int path, vec_bool stop_vertices_check, vec_vec_int graph);
+void write_solution_to_json(std::filesystem::path solution_path, vec_int solution, int value);
 
-std::ofstream res("przyklad1.txt");
-
-int main() {
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        std::cerr << "path to data file not provided\n";
+        return 1;
+    }
+    std::string test_data_path = argv[1];
     int n, s;
     vec_int stop_vertices;
     vec_vec_int graph;
-    read_data_from_json(FILENAME, n, s, graph, stop_vertices);
+    read_data_from_json(test_data_path, n, s, graph, stop_vertices);
     vec_bool stop_vertices_check(n, 0);
     for(auto &v : stop_vertices) {
         stop_vertices_check[v] = 1;
     }
     if(!is_connected(n, graph)) {
-        std::cout << "No solution: graph is not connected\n";
-    } else {
-        //std::cout << solve(n, s, graph, stop_vertices, stop_vertices_check) << '\n';
-    }
-    return(0);
+        std::cout << "-2 \n";
+        return 0;
+    }    
+    vec_int solution = solve(n, s, graph, stop_vertices, stop_vertices_check);
+    std::cout << max_subpath(solution, stop_vertices_check, graph) << '\n';
+    return 0;
 }
 
 vec_int solve(int n, int s, vec_vec_int &graph, vec_int &stop_vertices, vec_bool &stop_vertices_check) {
@@ -53,7 +56,7 @@ vec_int solve(int n, int s, vec_vec_int &graph, vec_int &stop_vertices, vec_bool
         check_all_possible_paths(1, 0, 0, 1, path, visited, min_max_subpath, opt_path, n, s, stop_vertices_check, graph);
     }
     if(min_max_subpath == INT_MAX) {
-        return vec_int(1,-1);
+        return vec_int(0);
     }
     return opt_path;
 }
@@ -118,8 +121,26 @@ void dfs(int n, int v, vec_vec_int& graph, std::vector<bool>& visited) {
     }
 }
 
-void read_data_from_json(std::string &filename, int &n, int &s, std::vector<std::vector<int>> &graph, std::vector<int> &stop_vertices) {
-    std::ifstream file(filename);
+int max_subpath(vec_int path, vec_bool stop_vertices_check, vec_vec_int graph) {
+    int max_subpath = 0;
+    int cur_subpath = 0;
+    if(!path.size()) {
+        return -1;
+    }
+    for(int i = 1; i < path.size(); ++i) {
+        cur_subpath += graph[path[i - 1]][path[i]];
+        if(stop_vertices_check[path[i]]) {
+            if(max_subpath < cur_subpath) {
+                max_subpath = cur_subpath;
+            }
+            cur_subpath = 0;
+        }
+    }
+    return max_subpath;
+}
+
+void read_data_from_json(std::filesystem::path test_data_path, int &n, int &s, std::vector<std::vector<int>> &graph, std::vector<int> &stop_vertices) {
+    std::ifstream file(test_data_path);
     if (file.is_open()) {
         json data;
         file >> data;
@@ -129,6 +150,19 @@ void read_data_from_json(std::string &filename, int &n, int &s, std::vector<std:
         stop_vertices = data["stop vertices"].get<std::vector<int>>();
         file.close();
     } else {
-        std::cerr << "Unable to open file '" << filename << "'." << '\n';
+        std::cerr << "Unable to open file '" << test_data_path << "'." << '\n';
+    }
+}
+
+void write_solution_to_json(std::filesystem::path solution_path, vec_int solution, int value) {
+    std::ofstream file(solution_path);
+    if (file.is_open()) {
+        json data;
+        data["solution"] = solution;
+        data["max subpath value"] = value;
+        file << data;
+        file.close();
+    } else {
+        std::cerr << "Unable to open file '" << solution_path << "'." << '\n';
     }
 }
